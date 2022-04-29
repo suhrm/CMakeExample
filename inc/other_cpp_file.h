d11eea1bb6b52a25703be7cfd4fe656bdf181c7e
suhrm <suhrminator@gmail.com>
2022-04-29 11:33:15 +0200

    updated example

--- inc/other_cpp_file.h
+++ inc/other_cpp_file.h
@@ -1,9 +1,6 @@
 #ifndef __other_cpp_file__
 #define __other_cpp_file__
 #include <iostream>
-
-void function_from_other_file() {
-	std::cout << "Print from other file" << std::endl;
-}
+void function_from_other_file();
 
 #endif
---
+++ src/other_cpp_file.cpp
@@ -0,0 +1,4 @@
+#include "other_cpp_file.h"
+void function_from_other_file() {
+	std::cout << "Print from other file" << std::endl;
+}
